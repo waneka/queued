d1db52a62108effde0b56cee4d5c7df3fba91fd9
maxdavila <adavila0711@gmail.com>
2013-10-18 12:16:28 -0700

    add static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Qd::Application.routes.draw do
+  root to: 'static_pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +40,7 @@ Qd::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
