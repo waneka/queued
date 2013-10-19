357f06259e05cd52ed3e5a2ae75b4023ff7148e6
Tyler <twwaneka@gmail.com>
2013-10-19 13:00:23 -0700

    adds jasmine test for adding song to queue

--- spec/scripts/applicationSpec.js
+++ spec/scripts/applicationSpec.js
@@ -6,14 +6,20 @@ describe("display search results", function(){
     container.className = 'container'
     document.body.appendChild(container)
 
-    $('<table>', {class: 'results-table'}).appendTo(container)
     $('<table>', {class: 'queue-table'}).appendTo(container)
+    $('<table>', {class: 'results-table'}).appendTo(container)
+    ViewController.displaySearchResults(mockQueryResponse)
   })
 
   it("should display correct number of results", function(){
     var numResults = mockQueryResponse.result.results.length
 
-    ViewController.displaySearchResults(mockQueryResponse)
     expect($('.results-table').find('tr')).toHaveLength(numResults)
   })
+
+  it("should add search result to queue", function(){
+    $('.result-row').first().find('button').trigger('click')
+    expect($('.queue-table').html()).toContain($('.result-row').first().html())
+  })
 })
+
