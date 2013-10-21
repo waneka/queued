fb3b12264484c5664ab300ffb60b0416597a1f85
Apprentice <apprentice@dbc-24.local>
2013-10-21 14:49:09 -0700

    migrate test and dev db to pg

--- db/migrate/20131021053202_create_table_parties.rb
+++
@@ -1,9 +0,0 @@
-class CreateTableParties < ActiveRecord::Migration
-  def change
-    create_table :table_parties do |t|
-      t.integer :user_id
-      t.string :url, :password
-      t.timestamps
-    end
-  end
-end
