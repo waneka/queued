90a5707371ea5535b80d31937d522e1c35466a80
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-10-21 19:41:07 -0700

    uncomment token

--- app/assets/javascripts/token.js
+++ app/assets/javascripts/token.js
@@ -1,7 +1,7 @@
 // LOCALHOST
-var playback_token = "GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=";
-var domain = "localhost";
+// var playback_token = "GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=";
+// var domain = "localhost";
 
 // HEROKU
-// var playback_token = "GBJSYx6a_____2R2cHlzNHd5ZXg3Z2M0OXdoaDY3aHdrbnFkZm0uaGVyb2t1YXBwLmNvbRfpDHKhoRPXY6ouG0vhV6g=";
-// var domain = "qdfm.herokuapp.com";
+var playback_token = "GBJSYx6a_____2R2cHlzNHd5ZXg3Z2M0OXdoaDY3aHdrbnFkZm0uaGVyb2t1YXBwLmNvbRfpDHKhoRPXY6ouG0vhV6g=";
+var domain = "qdfm.herokuapp.com";
