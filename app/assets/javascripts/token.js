aad0f73d9de9660d9aa1627913b2e6ffd6fb8a24
Lionel Ramos <lionelramos@Lionels-MacBook-Pro.local>
2013-10-22 11:36:22 -0700

    BUGFIX: adds valid token for localhost in token.js file

--- app/assets/javascripts/token.js
+++ app/assets/javascripts/token.js
@@ -1,7 +1,7 @@
 // LOCALHOST
-// var playback_token = "GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc";
-// var domain = "localhost";
+var playback_token = "GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=";
+var domain = "localhost";
 
 // HEROKU
-var playback_token = "GBNSZedd_____2R2cHlzNHd5ZXg3Z2M0OXdoaDY3aHdrbnFkLWZtLmhlcm9rdWFwcC5jb21YnJBBV3ZySa16sHUcl4cv";
-var domain = "qd-fm.herokuapp.com";
+// var playback_token = "GBNSZedd_____2R2cHlzNHd5ZXg3Z2M0OXdoaDY3aHdrbnFkLWZtLmhlcm9rdWFwcC5jb21YnJBBV3ZySa16sHUcl4cv";
+// var domain = "qd-fm.herokuapp.com";
