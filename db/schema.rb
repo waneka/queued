27bf45d616d3eb9ff485826b43c1ea79408ad70d
maxdavila <adavila0711@gmail.com>
2013-10-24 04:39:41 -0700

    lading page ready for R noon

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131021173153) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "parties", force: true do |t|
     t.integer  "user_id"
     t.string   "url"
